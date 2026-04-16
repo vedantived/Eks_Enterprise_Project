79aa31c6d8b568652c7fc49458398aa0ad93c37c
vedanti mardhekar <vedantim98@gmail.com>
2026-04-16 21:09:17 +0530

    Add ECR repository configuration for zero-trust-app

---
+++ environment/dev/ecr.tf
@@ -0,0 +1,9 @@
+resource "aws_ecr_repository" "app_repo" {
+  name = "zero-trust-app"
+
+  image_scanning_configuration {
+    scan_on_push = true             ##Every time an image is pushed AWS automatically scans it
+  }
+
+  image_tag_mutability = "MUTABLE"    ##Allows you to overwrite existing image tags
+}
