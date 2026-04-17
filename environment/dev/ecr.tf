d77bcd5a6562882eea55a90920a4d772cc7c8a5d
vedanti mardhekar <vedantim98@gmail.com>
2026-04-17 21:24:00 +0530

    Rename ECR repository from 'zero-trust-app' to 'eks-zero-trust-app'

--- environment/dev/ecr.tf
+++ environment/dev/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "app_repo" {
-  name = "zero-trust-app"
+  name = "eks-zero-trust-app"
 
   image_scanning_configuration {
     scan_on_push = true             ##Every time an image is pushed AWS automatically scans it
